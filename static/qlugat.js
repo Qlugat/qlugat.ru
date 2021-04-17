682bf366353567bfc4ec2daa71fde6275a89de4f
Serg Eresko <sergeresko@gmail.com>
2021-04-17 23:15:37 +0300

    Improve code formatting without any changes to the logic

--- static/qlugat.js
+++ static/qlugat.js
@@ -1,24 +1,27 @@
-var rusLetters = 'абвгдеёжзийклмнопрстуфхцчшщьыъэюя';
+var rusLetters = 'абвгдеёжзийклмнопрстуфхцчшщыъьэюя';
+
 function stemRu(word) {
     return word.replace(/ё/g, 'е');
 }
+
 function stemCrh(word) {
-    return word.replace(/ü/g, "u")
-        .replace(/ı/g, "i")
-        .replace(/ö/g, "o")
-        .replace(/â/g, "a")
-        .replace(/ş/g, "s")
-        .replace(/ğ/g, "g")
-        .replace(/ç/g, "c")
-        .replace(/ñ/g, "n")
-        .replace(/q/g, "k");
+    return word.replace(/ü/g, 'u')
+        .replace(/ı/g, 'i')
+        .replace(/ö/g, 'o')
+        .replace(/â/g, 'a')
+        .replace(/ş/g, 's')
+        .replace(/ğ/g, 'g')
+        .replace(/ç/g, 'c')
+        .replace(/ñ/g, 'n')
+        .replace(/q/g, 'k');
 }
+
 function getStem(word) {
     word = word.toLowerCase();
     if(rusLetters.indexOf(word[0]) !== -1) {
-	return stemRu(word);
+        return stemRu(word);
     } else {
-	return stemCrh(word);
+        return stemCrh(word);
     }
 }
 
@@ -32,17 +35,17 @@ function withStems(list) {
 var app = new Vue({
     el: '#app',
     data: {
-	start: true,
-	pos: -1,
-	word: '',
-	dictEntry: null,
-	list: [],
-	suggestDb: {}
+        start: true,
+        pos: -1,
+        word: '',
+        dictEntry: null,
+        list: [],
+        suggestDb: {}
     },
     methods: {
         submit: function (word) {
             fetch('/get_json?word=' + word).then(response => response.json()).then(data => {
-		this.dictEntry = (Object.keys(data).length === 0) ? null : data;
+                this.dictEntry = (Object.keys(data).length === 0) ? null : data;
             });
         },
         submitEnter: function(event) {
@@ -57,59 +60,59 @@ var app = new Vue({
           this.pos && this.pos--;
         },
         downHandler: function (event) {
-          event.preventDefault();
-          ((this.pos + 1) < this.list.length) && this.pos++;
+            event.preventDefault();
+            ((this.pos + 1) < this.list.length) && this.pos++;
         },
         wordChangedHandler: function(event) {
-	    this.start = false;
+            this.start = false;
             const word = event.target.value;
             const stem = getStem(word);
             if (word.length === 0) {
-		this.list = [];
-		this.pos = -1;
-		this.dictEntry = null;
+                this.list = [];
+                this.pos = -1;
+                this.dictEntry = null;
             } else if (word.length === 1 && !this.suggestDb[stem]) {
-		fetch('/suggest?token=' + word).then(response => response.json()).then(data => {
+                fetch('/suggest?token=' + word).then(response => response.json()).then(data => {
                     this.suggestDb[stem] = withStems(data);
                     this.updateList(getStem(this.word));
-		});
+                });
             } else {
-		var suggestList = this.suggestDb[stem[0]];
-		if (suggestList) {
+                var suggestList = this.suggestDb[stem[0]];
+                if (suggestList) {
                     this.updateList(stem);
-		}
+                }
             }
         },
-       updateList: function(stem) {
-           var suggestList = this.suggestDb[stem[0]];
-           this.list = stem ?
-	       suggestList.filter(item => {
-		   return item.stem.startsWith(stem)
-	       }).slice(0, 20) : [];
-           this.pos = 0;
-	   if (this.list.length == 1) {
-	       this.submit(this.list[0].word);
-	   }
-       },
-       itemClick: function(item, index) {
-          this.word = item;
-          this.pos = index;
-          this.dictEntry = null;
-          this.submit(item);
-       },
-	accentize: function(word, article) {
-	    var pos = article.accent_pos;
-	    if (pos) {
-		return word.substring(0, pos) + "\u0301" + word.substring(pos);
-	    } else {
-		return word;
-	    }
-	},
-	json2html: function(input, word, dictEntry) {
-	    var pos = dictEntry.shortening_pos;
-	    if (pos) {
-		input = input.replace(/~/g, word.substring(0, pos));
-	    }
+        updateList: function(stem) {
+            var suggestList = this.suggestDb[stem[0]];
+            this.list = stem ?
+                suggestList.filter(item => {
+                    return item.stem.startsWith(stem)
+                }).slice(0, 20) : [];
+            this.pos = 0;
+            if (this.list.length == 1) {
+                this.submit(this.list[0].word);
+            }
+        },
+        itemClick: function(item, index) {
+            this.word = item;
+            this.pos = index;
+            this.dictEntry = null;
+            this.submit(item);
+        },
+        accentize: function(word, article) {
+            var pos = article.accent_pos;
+            if (pos) {
+                return word.substring(0, pos) + "\u0301" + word.substring(pos);
+            } else {
+                return word;
+            }
+        },
+        json2html: function(input, word, dictEntry) {
+            var pos = dictEntry.shortening_pos;
+            if (pos) {
+                input = input.replace(/~/g, word.substring(0, pos));
+            }
 //            input = input.replace(/^(.[^)].+? )-( .+?)$/mg,'<span class="example">$1─$2</span>');
 //            input = input.replace(/\/(.+?)\//g,'<i class="spec">$1</i>');
 //            input = input.replace(/^(ср|см)\. (.+)$/mg,
@@ -122,12 +125,11 @@ var app = new Vue({
 //                    return '<i class="link">'+p1+'.</i> '+links.slice(0, -2);
 //                });
             input = input.replace(/(лингв|перен|физ|хим|бот|биол|зоо|грам|геогр|астр|шк|мат|анат|ирон|этн|стр|рел|посл|уст)\./g,'<i class="spec">$&</i>');
-            input = input.replace('◊','\n◊\n');
-	    input = input.replace(/\\n/g,'<br/>');
-	    input = input.replace(/\n/g,'<br/>');
-            input = input.replace(/; /g,'<br/>');
+            input = input.replace('◊', '\n◊\n');
+            input = input.replace(/\\n/g, '<br/>');
+            input = input.replace(/\n/g, '<br/>');
+            input = input.replace(/; /g, '<br/>');
             return input;
-      }
+        }
     }
 })
-
